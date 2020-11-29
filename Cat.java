4850d5c0fdcadd47a52c5effe8d63f450047e9dc
danoon3 <73851394+danoon3@users.noreply.github.com>
2020-11-29 12:40:59 +0300

    Delete Cat.java

--- Cat.java
+++
@@ -1,9 +0,0 @@
-package ru.geekbrains.java_one.lesson_five;
-
-import ru.geekbrains.java_one.lesson_five.Animals;
-
-public class Cat extends Animals {
-    public Cat(String name, float maximumJump, float maximumRun, float maximumSwim){
-        super(name, maximumJump, maximumRun, maximumSwim);
-    }
-}
